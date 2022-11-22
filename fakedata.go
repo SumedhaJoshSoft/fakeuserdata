4fc5562b4956fbda03e5a8f028b5b569d802c3dd
SumedhaJoshSoft <sumedha.jagtap@joshsoftware.com>
2022-11-22 15:19:47 +0530

    fake user module

---
+++ fakedata.go
@@ -0,0 +1,25 @@
+package fakedata
+
+import (
+	"fmt"
+
+	"github.com/bxcodec/faker/v3"
+)
+
+type User struct {
+	FirstName   string `faker:"first_name"`
+	LastName    string `faker:"last_name"`
+	PhoneNumber string `faker:"phone_number"`
+	Email       string `faker:"email"`
+	Password    string `faker:"password"`
+}
+
+func fakeuser() (data *User) {
+	err := faker.FakeData(&data)
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+	return data
+
+}
